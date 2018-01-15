ed259b0936a23bdc3311be259b9e5e37b2ebcc84
Mark Howells-Mead <mark@cubetech.ch>
2018-01-15 14:03:09 +0100

    Grammatical changes, some PSR spacing changes

--- Classes/class-manifest.php
+++ Classes/class-manifest.php
@@ -2,42 +2,46 @@
 
 namespace nicomartin\ProgressiveWordPress;
 
-class Manifest {
+class Manifest
+{
 
 	public $capability = '';
 	public $manifest_path = ABSPATH . 'manifest.json';
 	public $manifest_url = '/manifest.json';
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this->capability = pwp_get_instance()->Init->capability;
 	}
 
-	public function run() {
-		add_action( 'pwp_settings', [ $this, 'register_settings' ] );
-		add_action( 'pwp_on_sanitize', [ $this, 'save_manifest' ] );
-		if ( file_exists( $this->manifest_path ) ) {
-			add_action( 'wp_head', [ $this, 'add_to_header' ], 1 );
+	public function run()
+	{
+		add_action('pwp_settings', [ $this, 'register_settings' ]);
+		add_action('pwp_on_sanitize', [ $this, 'save_manifest' ]);
+		if (file_exists($this->manifest_path)) {
+			add_action('wp_head', [ $this, 'add_to_header' ], 1);
 		}
 	}
 
-	public function register_settings() {
+	public function register_settings()
+	{
 
-		$section_desc = '<b>' . __( 'Make your website installable!', 'pwp' ) . '</b><br>';
-		$section_desc .= __( 'This values are used to create a manifest.json which provides the data to make your website installable.', 'pwp' );
+		$section_desc = '<b>' . __('Make your website installable!', 'pwp') . '</b><br>';
+		$section_desc .= __('This values are used to create a manifest.json file, which provides the data to make your website installable.', 'pwp');
 		$url          = 'https://developer.mozilla.org/de/docs/Web/Manifest';
-		$section_desc .= '<br>' . __( 'Read all about the web app manifest:.', 'pwp' ) . ' <a target="_blank" href="' . $url . '">' . $url . '</a>';
-		$section      = pwp_settings()->add_section( pwp_settings_page_manifest(), 'pwp_manifest', __( 'Manifest.json values', 'pwp' ), $section_desc );
+		$section_desc .= '<br>' . __('Read all about the web app manifest:', 'pwp') . ' <a target="_blank" href="' . $url . '">' . $url . '</a>';
+		$section      = pwp_settings()->add_section(pwp_settings_page_manifest(), 'pwp_manifest', __('Manifest.json values', 'pwp'), $section_desc);
 
-		pwp_settings()->add_checkbox( $section, 'manifest-enabled', __( 'Manifest enabled', 'pwp' ), true, [
-			'after_field' => '<p class="pwp-smaller">' . __( 'Adds the manifest to the wp_head.', 'pwp' ) . '</p>',
-		] );
-		pwp_settings()->add_input( $section, 'manifest-name', __( 'Name', 'pwp' ) );
-		pwp_settings()->add_input( $section, 'manifest-short-name', __( 'Short Name', 'pwp' ), '', [
-			'after_field' => '<p class="pwp-smaller">' . __( 'max. 12 Chars', 'pwp' ) . '</p>',
-		] );
+		pwp_settings()->add_checkbox($section, 'manifest-enabled', __('Manifest enabled', 'pwp'), true, [
+			'after_field' => '<p class="pwp-smaller">' . __('Adds the manifest to the wp_head.', 'pwp') . '</p>',
+		]);
+		pwp_settings()->add_input($section, 'manifest-name', __('Name', 'pwp'));
+		pwp_settings()->add_input($section, 'manifest-short-name', __('Short Name', 'pwp'), '', [
+			'after_field' => '<p class="pwp-smaller">' . __('max. 12 Chars', 'pwp') . '</p>',
+		]);
 
-		pwp_settings()->add_textarea( $section, 'manifest-description', __( 'Description', 'pwp' ), '', [] );
-		pwp_settings()->add_file( $section, 'manifest-icon', __( 'Icon', 'pwp' ) );
+		pwp_settings()->add_textarea($section, 'manifest-description', __('Description', 'pwp'), '', []);
+		pwp_settings()->add_file($section, 'manifest-icon', __('Icon', 'pwp'));
 
 		$choices = [
 			'fullscreen' => 'Fullscreen',
@@ -45,55 +49,57 @@ class Manifest {
 			'minimal-ui' => 'Minimal',
 		];
 		$link    = 'https://developer.mozilla.org/de/docs/Web/Manifest#display';
-		pwp_settings()->add_select( $section, 'manifest-display', __( 'Display mode', 'pwp' ), $choices, 'standalone', [
-			'after_field' => '<p class="pwp-smaller">' . __( 'possible display modes', 'pwp' ) . ": <a href='$link' target='_blank'>$link</a></p>",
-		] );
+		pwp_settings()->add_select($section, 'manifest-display', __('Display mode', 'pwp'), $choices, 'standalone', [
+			'after_field' => '<p class="pwp-smaller">' . __('possible display modes', 'pwp') . ": <a href='$link' target='_blank'>$link</a></p>",
+		]);
 
 		$args = [
-			'after_field' => '<p class="pwp-smaller">' . __( 'hex value', 'pwp' ) . '</p>',
+			'after_field' => '<p class="pwp-smaller">' . __('hex value', 'pwp') . '</p>',
 		];
-		pwp_settings()->add_input( $section, 'manifest-theme-color', __( 'Theme Color', 'pwp' ), '', $args );
-		pwp_settings()->add_input( $section, 'manifest-background-color', __( 'Background Color', 'pwp' ), '', $args );
+		pwp_settings()->add_input($section, 'manifest-theme-color', __('Theme Color', 'pwp'), '', $args);
+		pwp_settings()->add_input($section, 'manifest-background-color', __('Background Color', 'pwp'), '', $args);
 	}
 
-	public function save_manifest( $data ) {
-		if ( ! array_key_exists( 'manifest-name', $data ) ) {
+	public function save_manifest($data)
+	{
+		if (! array_key_exists('manifest-name', $data)) {
 			return;
 		}
 
 		$manifest                     = [];
 		$manifest['name']             = $data['manifest-name'];
-		$manifest['short_name']       = str_replace( ' ', '', $data['manifest-short-name'] );
+		$manifest['short_name']       = str_replace(' ', '', $data['manifest-short-name']);
 		$manifest['start_url']        = './';
 		$manifest['description']      = $data['manifest-description'];
-		$manifest['theme_color']      = sanitize_hex_color( $data['manifest-theme-color'] );
-		$manifest['background_color'] = sanitize_hex_color( $data['manifest-background-color'] );
+		$manifest['theme_color']      = sanitize_hex_color($data['manifest-theme-color']);
+		$manifest['background_color'] = sanitize_hex_color($data['manifest-background-color']);
 		$manifest['display']          = $data['manifest-display'];
 		$manifest['lang']             = '';
 		$manifest['orientation']      = 'any';
 
 		$sizes = [ 128, 512 ];
-		if ( array_key_exists( 'manifest-icon', $data ) && wp_attachment_is_image( intval( $data['manifest-icon'] ) ) ) {
-			$mime = get_post_mime_type( $data['manifest-icon'] );
-			foreach ( $sizes as $size ) {
+		if (array_key_exists('manifest-icon', $data) && wp_attachment_is_image(intval($data['manifest-icon']))) {
+			$mime = get_post_mime_type($data['manifest-icon']);
+			foreach ($sizes as $size) {
 				$manifest['icons'][] = [
-					'src'   => pwp_register_url( pwp_get_instance()->image_resize( $data['manifest-icon'], $size, $size, true )['url'] ),
+					'src'   => pwp_register_url(pwp_get_instance()->image_resize($data['manifest-icon'], $size, $size, true)['url']),
 					'type'  => $mime,
 					'sizes' => "{$size}x{$size}",
 				];
 			}
 		}
 
-		$content = json_encode( $manifest, JSON_UNESCAPED_SLASHES );
-		file_put_contents( $this->manifest_path, $content );
+		$content = json_encode($manifest, JSON_UNESCAPED_SLASHES);
+		file_put_contents($this->manifest_path, $content);
 	}
 
-	public function add_to_header() {
-		if ( ! pwp_get_setting( 'manifest-enabled' ) ) {
+	public function add_to_header()
+	{
+		if (! pwp_get_setting('manifest-enabled')) {
 			return;
 		}
 
-		$url = untrailingslashit( get_home_url() ) . $this->manifest_url;
-		echo '<link rel="manifest" href="' . pwp_register_url( $url ) . '">';
+		$url = untrailingslashit(get_home_url()) . $this->manifest_url;
+		echo '<link rel="manifest" href="' . pwp_register_url($url) . '">';
 	}
 }
--- Classes/class-offlineusage.php
+++ Classes/class-offlineusage.php
@@ -2,72 +2,76 @@
 
 namespace nicomartin\ProgressiveWordPress;
 
-class Offlineusage {
+class Offlineusage
+{
 
 	public $capability = '';
 	public $indicator_text = '';
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this->capability     = pwp_get_instance()->Init->capability;
 		$this->indicator_text = 'you\'re currently offline';
 	}
 
-	public function run() {
-		add_action( 'pwp_settings', [ $this, 'settings' ] );
-		add_action( 'pwp_settings', [ $this, 'offline_indicator_settings' ] );
-		add_action( 'wp_footer', [ $this, 'offline_indicator_template' ] );
+	public function run()
+	{
+		add_action('pwp_settings', [ $this, 'settings' ]);
+		add_action('pwp_settings', [ $this, 'offline_indicator_settings' ]);
+		add_action('wp_footer', [ $this, 'offline_indicator_template' ]);
 	}
 
-	public function settings() {
+	public function settings()
+	{
 
-		$section_desc = __( 'This feature allows you to provide offline usage for your website.', 'pwp' ) . '<br>';
-		$section_desc .= __( 'A copy of each pages is stored in the cache as your visitors view them. This allows a visitor to load any previously viewed page while they are offline. This then adds the "offline page" that allows you to customize the message and experience if the app is offline, and the page is not in the cache.', 'pwp' );
-		$section      = pwp_settings()->add_section( pwp_settings_page_offlineusage(), 'pwp_offlineusage', __( 'Offline Usage', 'pwp' ), $section_desc );
+		$section_desc = __('This feature allows you to provide offline usage for your website.', 'pwp') . '<br>';
+		$section_desc .= __('A copy of each page is stored in the browser cache as the visitor views it. This allows a visitor to load any previously viewed page while they are offline.  The plugin also defines a special “offline page”, which allows you to customize a message and the experience if the app is offline and the page is not in the cache.', 'pwp');
+		$section      = pwp_settings()->add_section(pwp_settings_page_offlineusage(), 'pwp_offlineusage', __('Offline Usage', 'pwp'), $section_desc);
 
-		pwp_settings()->add_checkbox( $section, 'offline-enabled', __( 'Offline Usage enabled', 'pwp' ) );
+		pwp_settings()->add_checkbox($section, 'offline-enabled', __('Offline Usage enabled', 'pwp'));
 
 		$choices = [];
-		foreach ( get_pages() as $post ) {
-			$choices[ $post->ID ] = get_the_title( $post );
+		foreach (get_pages() as $post) {
+			$choices[ $post->ID ] = get_the_title($post);
 		}
-		pwp_settings()->add_select( $section, 'offline-page', __( 'Offline Page', 'pwp' ), $choices );
+		pwp_settings()->add_select($section, 'offline-page', __('Offline Page', 'pwp'), $choices);
 	}
 
-	public function offline_indicator_settings() {
-		$section = pwp_settings()->add_section( pwp_settings_page_offlineusage(), 'pwp_offlineindicator', __( 'offline indicator  ', 'pwp' ) );
-		pwp_settings()->add_checkbox( $section, 'offline-indicator', __( 'Offline indicator', 'pwp' ), false );
-		pwp_settings()->add_input( $section, 'offline-indicator-text', __( 'Message', 'pwp' ), $this->indicator_text );
-		pwp_settings()->add_select( $section, 'offline-indicator-position', __( 'Position', 'pwp' ), [
-			'bottom' => __( 'Bottom', 'pwp' ),
-			'top'    => __( 'Top', 'pwp' ),
-		], 'bottom' );
+	public function offline_indicator_settings()
+	{
+		$section = pwp_settings()->add_section(pwp_settings_page_offlineusage(), 'pwp_offlineindicator', __('offline indicator  ', 'pwp'));
+		pwp_settings()->add_checkbox($section, 'offline-indicator', __('Offline indicator', 'pwp'), false);
+		pwp_settings()->add_input($section, 'offline-indicator-text', __('Message', 'pwp'), $this->indicator_text);
+		pwp_settings()->add_select($section, 'offline-indicator-position', __('Position', 'pwp'), [
+			'bottom' => __('Bottom', 'pwp'),
+			'top'    => __('Top', 'pwp'),
+		], 'bottom');
 
-		pwp_settings()->add_input( $section, 'offline-indicator-color-text', __( 'Textcolor', 'pwp' ), '#ffffff', [
-			'after_field' => __( 'Hex value', 'pwp' ),
-		] );
-		pwp_settings()->add_input( $section, 'offline-indicator-color-background', __( 'Background-Color', 'pwp' ), '#000000', [
-			'after_field' => __( 'Hex value', 'pwp' ),
-		] );
-
+		pwp_settings()->add_input($section, 'offline-indicator-color-text', __('Textcolor', 'pwp'), '#ffffff', [
+			'after_field' => __('Hex value', 'pwp'),
+		]);
+		pwp_settings()->add_input($section, 'offline-indicator-color-background', __('Background-Color', 'pwp'), '#000000', [
+			'after_field' => __('Hex value', 'pwp'),
+		]);
 	}
 
-	public function offline_indicator_template() {
+	public function offline_indicator_template()
+	{
 
-		$indicator = pwp_settings()->get_setting( 'offline-indicator' );
+		$indicator = pwp_settings()->get_setting('offline-indicator');
 
-		if ( ! $indicator ) {
+		if (! $indicator) {
 			return;
 		}
 
-		$text     = pwp_settings()->get_setting( 'offline-indicator-text' );
-		$position = pwp_settings()->get_setting( 'offline-indicator-position' );
+		$text     = pwp_settings()->get_setting('offline-indicator-text');
+		$position = pwp_settings()->get_setting('offline-indicator-position');
 
-		$textcolor = pwp_settings()->get_setting( 'offline-indicator-color-text' );
-		$bkgcolor  = pwp_settings()->get_setting( 'offline-indicator-color-background' );
+		$textcolor = pwp_settings()->get_setting('offline-indicator-color-text');
+		$bkgcolor  = pwp_settings()->get_setting('offline-indicator-color-background');
 
 		echo "<div class='offline-indicator offline-indicator--$position' style='background-color: $bkgcolor'>";
 		echo "<p style='color:$textcolor'>$text</p>";
 		echo '</div>';
-
 	}
 }
