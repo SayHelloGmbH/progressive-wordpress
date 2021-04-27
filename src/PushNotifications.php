dfe3c65ffd515b8495ecffce0f0f928b30465ae2
Nico Martin <mail@nicomartin.ch>
2021-04-27 15:12:10 +0200

    WIP push frontend functions

--- src/PushNotifications.php
+++ src/PushNotifications.php
@@ -8,12 +8,6 @@ class PushNotifications
     {
         add_filter('pwp_register_settings', [$this, 'settings']);
         add_filter('pwp_admin_footer_js', [$this, 'footerJs']);
-        /*add_filter('pwp_push_use_firebase', function () {
-            return true;
-AAAAk9pstwM:APA91bGdai_bzip_f-TBa3RH3PEBJyl1sL4hWUd3jfK0B1suQ-hdi3Sn0zOb4JnMJi_GmbE5meQSLgIoAWonLumf6Ncd2YRM0Fyr9yTeuJNsidsyp8BEI7VvjOUPGKZnaqg49E9KMRkA
-635024750339
-            return false;
-        });*/
         add_filter('pwp_push_use_firebase', function () {
             return true;
         });
